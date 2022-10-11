9c6f92359744aedf552937e6c01f55cde394948d
Atommerce <atommerce@Atommerceui-MacBookPro-2.local>
2022-10-11 10:03:12 +0900

    project setting

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheHeader from '@/layouts/TheHeader.vue';
+import TheView from '@/layouts/TheView.vue';
+</script>
+
+<template>
+  <div>
+    <TheHeader></TheHeader>
+    <TheView></TheView>
+  </div>
+</template>
+
+<style scoped></style>
